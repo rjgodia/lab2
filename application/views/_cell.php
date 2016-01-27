ef71f4262ff0a594055501dd4fb612412bca850c
rjgodia <ahrjhey.godia@yahoo.com>
2016-01-26 17:28:45 -0800

    part 3

---
+++ application/views/_cell.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rjgodia
+ * Date: 2016-01-26
+ * Time: 4:35 PM
+ */
+
+//<a href="./data/serengeti-landscape.jpg" data-lightbox="gallery" data-title="Beautiful Landscape of Serengeti ... Posted 2014.04.05 by donald, in landscape"><img src="./data/thumb/serengeti-landscape.jpg"/></a>
+
+?>
+
+<a href="./data/{filename}" data-lightbox="gallery"
+   data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+    <img src= "./data/thumb/{filename}"/>
+</a>
